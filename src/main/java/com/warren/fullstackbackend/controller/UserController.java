5523e1503594ae400a3923bcfe15e80a535697ec
Warren Chan <warren.chan0113@gmail.com>
2024-03-06 17:08:44 -0500

    finish post and get

--- src/main/java/com/warren/fullstackbackend/controller/UserController.java
+++ src/main/java/com/warren/fullstackbackend/controller/UserController.java
@@ -1,7 +1,26 @@
 package com.warren.fullstackbackend.controller;
-
+import com.warren.fullstackbackend.model.User;
+import com.warren.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController // used to create RESTful web services using Spring MVC.
 public class UserController {
+    @Autowired // allows Spring to automatically inject dependencies into the class
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
