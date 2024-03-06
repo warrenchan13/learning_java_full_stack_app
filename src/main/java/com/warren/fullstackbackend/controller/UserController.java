e1d8258f95b9e609bb080b23f0efa47f9ebafc75
Warren Chan <warren.chan0113@gmail.com>
2024-03-06 16:52:13 -0500

    finish user model

---
+++ src/main/java/com/warren/fullstackbackend/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.warren.fullstackbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // used to create RESTful web services using Spring MVC.
+public class UserController {
+}
--- src/main/java/com/warren/fullstackbackend/model/User.java
+++ src/main/java/com/warren/fullstackbackend/model/User.java
@@ -1,4 +1,47 @@
 package com.warren.fullstackbackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity // This will automatically generate User table when connected to the db
 public class User {
+    @Id // annotation designates that this member will uniquely identify the entity in the database.
+    @GeneratedValue // annotation is used to specify how the primary key values of an entity are generated.
+    private Long id;
+    private String username;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
